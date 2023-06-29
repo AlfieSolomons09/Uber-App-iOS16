151394c10ee4bc4ac975f43bc6ff1a1c2c477fd3
Chetan Choudhary <98541541+AlfieSolomons09@users.noreply.github.com>
2023-06-29 15:34:58 +0530

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Chetan Choudhary on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
